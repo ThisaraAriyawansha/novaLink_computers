92580be4e8ca8fab17c8cf78f12b61e51bb9f7ab
ThisaraAriyawansha <thisara.a2001@gmail.com>
2025-08-05 16:35:07 +0530

    update

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -279,6 +279,21 @@
         @include('layouts.footer2')
     </div>
 
+    <!-- JS Files
+    ============================================ -->
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="assets/js/plugins/jquery.countdown.min.js"></script>
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/scrollUp.js"></script>
+    <script src="assets/js/plugins/venobox.min.js"></script>
+    <script src="assets/js/plugins/jquery-ui.min.js"></script>
+    <script src="assets/js/plugins/mailchimp-ajax.js"></script>
+    <!--Main JS (Common Activation Codes)-->
+    <script src="assets/js/main.js"></script>
+
         <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
     <!-- Scripts -->
     <script>
